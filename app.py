c50b96ba272fd241d67e247a78a02c9b42dd09ab
mari-code-l <maridel.arcagua@gmail.com>
2021-04-24 18:21:57 +0000

    Add add_recipes.html and route

--- app.py
+++ app.py
@@ -96,6 +96,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_recipes")
+def add_recipes():
+    return render_template("add_recipes")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
